07d99e68fd7124767ff09d8370ed69f3a7659fe5
levelup-glitch <konakahanika46@gmail.com>
2025-05-22 15:44:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def download():
     except Exception as e:
         return str(e), 500
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
